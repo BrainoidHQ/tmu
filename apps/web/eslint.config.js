3e3f8e892fd5ec368e3b92140cf2f6e6f9ae88a9
yoseio <98276492+yoseio@users.noreply.github.com>
2024-12-25 08:22:25 +0000

    chore: update

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,6 +1,17 @@
 import react from "@brainoid-tmu/eslint/react";
 import ts from "typescript-eslint";
 
-export default ts.config(react, {
-  ignores: ["eslint.config.js", "postcss.config.js"],
-});
+export default ts.config(
+  react,
+  { ignores: ["eslint.config.js", "postcss.config.js"] },
+  {
+    rules: {
+      "@typescript-eslint/prefer-promise-reject-errors": [
+        "error",
+        {
+          allowThrowingUnknown: true,
+        },
+      ],
+    },
+  },
+);
