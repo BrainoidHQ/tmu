00005347878aaf5a78febffa70f5609e72f653e2
Kousuke Takaki <98276492+yoseio@users.noreply.github.com>
2024-12-26 19:50:58 +0000

    chore(crawler): use CheerioCrawler

--- apps/crawler/src/main.ts
+++ apps/crawler/src/main.ts
@@ -1,26 +1,15 @@
 // For more information, see https://crawlee.dev/
-import { PlaywrightCrawler } from "crawlee";
+import { CheerioCrawler } from "crawlee";
 
-// PlaywrightCrawler crawls the web using a headless
-// browser controlled by the Playwright library.
-const crawler = new PlaywrightCrawler({
-  // Use the requestHandler to process each of the crawled pages.
-  async requestHandler({ request, page, enqueueLinks, log, pushData }) {
-    const title = await page.title();
-    log.info(`Title of ${request.loadedUrl} is '${title}'`);
+import { router } from "./routes";
 
-    // Save results as JSON to ./storage/datasets/default
-    await pushData({ title, url: request.loadedUrl });
+const startUrls = ["https://crawlee.dev"];
 
-    // Extract links from the current page
-    // and add them to the crawling queue.
-    await enqueueLinks();
-  },
+const crawler = new CheerioCrawler({
+  // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+  requestHandler: router,
   // Comment this option to scrape the full website.
   maxRequestsPerCrawl: 20,
-  // Uncomment this option to see the browser window.
-  // headless: false,
 });
 
-// Add first URL to the queue and start the crawl.
-await crawler.run(["https://crawlee.dev"]);
+await crawler.run(startUrls);
